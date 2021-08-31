a40d13e96136a5466a05e55a0223febd02f1690b
buxkirill <73946874+buxkirill@users.noreply.github.com>
2021-08-31 22:47:59 +0500

    Create lesson_7_homework.sql

---
+++ SQL/homeworks/lesson_7_homework.sql
@@ -0,0 +1,19 @@
+--colab/jupyter: https://colab.research.google.com/drive/1j4XdGIU__NYPVpv74vQa9HUOAkxsgUez?usp=sharing
+
+--task1  (lesson7)
+-- sqlite3: Сделать тестовый проект с БД (sqlite3, project name: task1_7). В таблицу table1 записать 1000 строк с случайными значениями (3 колонки, тип int) от 0 до 1000.
+-- Далее построить гистаграмму распределения этих трех колонко
+
+ok
+
+--task2  (lesson7)
+-- oracle: https://leetcode.com/problems/duplicate-emails/
+
+--task3  (lesson7)
+-- oracle: https://leetcode.com/problems/employees-earning-more-than-their-managers/
+
+--task4  (lesson7)
+-- oracle: https://leetcode.com/problems/rank-scores/
+
+--task5  (lesson7)
+-- oracle: https://leetcode.com/problems/combine-two-tables/
