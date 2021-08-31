2ebbcd667d0b46895c9f49696ebd62b18aaafa26
buxkirill <73946874+buxkirill@users.noreply.github.com>
2021-08-31 22:54:29 +0500

    Update lesson_7_homework.sql

--- SQL/homeworks/lesson_7_homework.sql
+++ SQL/homeworks/lesson_7_homework.sql
@@ -9,8 +9,24 @@ ok
 --task2  (lesson7)
 -- oracle: https://leetcode.com/problems/duplicate-emails/
 
+# Write your MySQL query statement below
+select email
+from (
+    select *,
+        row_number() over(partition by email) as row_num
+    from person
+) as t1
+where row_num = 2;
+
 --task3  (lesson7)
 -- oracle: https://leetcode.com/problems/employees-earning-more-than-their-managers/
+
+# Write your MySQL query statement below
+select t1.name as employee
+from Employee t1
+left outer join Employee t2 on t1.managerid=t2.id
+where t1.salary > t2.salary
+
 
 --task4  (lesson7)
 -- oracle: https://leetcode.com/problems/rank-scores/
