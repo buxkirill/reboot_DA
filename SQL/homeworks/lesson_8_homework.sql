758fd52dcc57a2fb7b966fff3b93716d057892eb
buxkirill <73946874+buxkirill@users.noreply.github.com>
2021-09-02 22:58:09 +0500

    Create lesson_8_homework.sql

---
+++ SQL/homeworks/lesson_8_homework.sql
@@ -0,0 +1,44 @@
+--task1  (lesson8)
+-- oracle: https://leetcode.com/problems/department-top-three-salaries/
+
+--task2  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/17
+
+--task3  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/13
+
+--task4  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/38
+
+--task5  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/35
+
+--task6  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/38
+
+--task7  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/32
+
+--task8  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/27
+
+--task9  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/37
+
+--task10  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/44
+
+--task11 (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/20
+
+--task12  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/55
+
+--task13  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/45
+
+--task14  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/43
+
+--task15  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/63
