d17403127e641a82585e8425e91888c917b9c714
buxkirill <73946874+buxkirill@users.noreply.github.com>
2021-09-08 22:01:00 +0500

    Update lesson_9_homework.sql

--- SQL/homeworks/lesson_9_homework.sql
+++ SQL/homeworks/lesson_9_homework.sql
@@ -69,11 +69,57 @@ order by grade desc, name, marks asc;
 --task2  (lesson9)
 -- oracle: https://www.hackerrank.com/challenges/occupations/problem
 
+with query1 as (
+    select 
+        name, 
+        occupation,
+        row_number() over(order by name) as row_num
+    from OCCUPATIONS
+    where upper(occupation) like '%PROFESSOR%'
+),
+query2 as (
+    select 
+        name, 
+        occupation,
+        row_number() over(order by name) as row_num
+    from OCCUPATIONS
+    where upper(occupation) like '%DOCTOR%'
+),
+query3 as (
+    select 
+        name, 
+        occupation,
+        row_number() over(order by name) as row_num
+    from OCCUPATIONS
+    where upper(occupation) like '%SINGER%'
+),
+query4 as (
+    select 
+        name, 
+        occupation,
+        row_number() over(order by name) as row_num
+    from OCCUPATIONS
+    where upper(occupation) like '%ACTOR%'
+)
+select t2.name, t1.name, t3.name, t4.name
+from query1 t1
+full outer join query2 t2 on t1.row_num=t2.row_num
+full outer join query3 t3 on t1.row_num=t3.row_num
+full outer join query4 t4 on t1.row_num=t4.row_num;
+
 --task3  (lesson9)
 -- oracle: https://www.hackerrank.com/challenges/weather-observation-station-9/problem
 
+select distinct city 
+from STATION 
+where upper(substr(city, 0, 1)) not in ('E', 'U', 'I', 'O', 'A');
+
 --task4  (lesson9)
 -- oracle: https://www.hackerrank.com/challenges/weather-observation-station-10/problem
+
+select distinct city
+from station
+where upper(substr(city, -1, 1)) not in ('E', 'U', 'I', 'O', 'A');
 
 --task5  (lesson9)
 -- oracle: https://www.hackerrank.com/challenges/weather-observation-station-11/problem
